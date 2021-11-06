056b16d925f5b3aaff2f5c4f0f6abc8c8fa22ae2
comfortliner <andreas_werz@gmx.de>
2021-11-06 07:50:32 +0100

    feat(calculator): add 'sub' function

--- test/unit/calculatorTests.js
+++ test/unit/calculatorTests.js
@@ -21,4 +21,21 @@ suite('calculator', () => {
       done();
     });
   });
+
+  suite('sub', () => {
+    test('return the substraction of the given number.', done => {
+      const actual = calculator.sub(27, 13),
+            expected = 14;
+
+      assert.that(actual).is.equalTo(expected);
+      done();
+    });
+
+    test('throws an error if numbers are missing.', done => {
+      assert.that(() => {
+        calculator.sub();
+      }).is.throwing('Numbers are missing.');
+      done();
+    });
+  });
 });
